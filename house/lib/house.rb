7a6856aa42275e4b0346ec390581de8d277969a9
Sandi Metz <sandi@sandimetz.com>
2017-07-12 12:47:48 +0200

    Revert to initial House

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -1,37 +1,3 @@
 class House
-  DATA = [
-    'the horse and the hound and the horn that belonged to',
-    'the farmer sowing his corn that kept',
-    'the rooster that crowed in the morn that woke',
-    'the priest all shaven and shorn that married',
-    'the man all tattered and torn that kissed',
-    'the maiden all forlorn that milked',
-    'the cow with the crumpled horn that tossed',
-    'the dog that worried',
-    'the cat that killed',
-    'the rat that ate',
-    'the malt that lay in',
-    'the house that Jack built']
 
-  attr_reader :data
-
-  def initialize
-    @data = DATA
-  end
-
-  def recite
-    1.upto(12).collect {|i| line(i)}.join("\n")
-  end
-
-  def line(num)
-    "#{prefix} #{phrase(num)}.\n"
-  end
-
-  def prefix
-    "This is"
-  end
-
-  def phrase(num)
-    data.last(num).join(' ')
-  end
 end
