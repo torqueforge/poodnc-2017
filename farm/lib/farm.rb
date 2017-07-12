1b2f6dea1db7b15d538470f495fc0a9295a94625
Sandi Metz <sandi@sandimetz.com>
2017-07-12 12:59:02 +0200

    farm with enforcement

--- farm/lib/farm.rb
+++ farm/lib/farm.rb
@@ -14,6 +14,7 @@ class Farm
   def verse(animal)
     sound   = animal.sound
     species = animal.species
+
     "Old MacDonald had a farm, E-I-E-I-O,\n" +
     "And on that farm he had #{species.articlize}, E-I-E-I-O,\n" +
     "With #{sound.articlize} #{sound} here " +
@@ -21,5 +22,43 @@ class Farm
     "Here #{sound.articlize}, there #{sound.articlize}, " +
       "everywhere #{sound.articlize} #{sound},\n" +
     "Old MacDonald had a farm, E-I-E-I-O."
+  end
+end
+
+class NullAnimal
+  def sound
+    "<silence>"
+  end
+
+  def species
+    "<silence>"
+  end
+end
+
+class MyAnimal
+  def self.all(ids)
+    Animal.all(ids).collect {|animal| animal || NullAnimal.new}
+  end
+end
+
+
+# The following ensures that my wrapper is called instead of original method
+require 'pathname'
+module Animal
+  singleton_class.send(:alias_method, :orig_all, :all)
+
+  def self.all(ids)
+    validate_caller(caller_locations(1,1).first)
+    orig_all(ids)
+  end
+
+  def self.validate_caller(caller)
+    actual_calling_file = Pathname.new(caller.path).basename.to_s
+    expected_calling_file = "farm.rb"
+
+    msg = "Animal.all may only be called by MyAnimal (defined in #{expected_calling_file})." +
+          "Please change 'Anmial.all' to 'MyAnimal.all' in #{actual_calling_file}) "
+
+    raise "Error: #{msg}" unless actual_calling_file == expected_calling_file
   end
 end
