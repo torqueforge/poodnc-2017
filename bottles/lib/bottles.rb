b28d627c8ef384fbcc2a1b1af5c06f807c69d4a9
Sandi Metz <sandi@sandimetz.com>
2017-09-29 09:41:54 -0700

    tentative clio day 3

---
+++ bottles/lib/bottles.rb
@@ -0,0 +1,57 @@
+class Bottles
+
+  def song
+    verses(99, 0)
+  end
+
+  def verses(upper, lower)
+    upper.downto(lower).map { |i| verse(i) }.join("\n")
+  end
+
+  def verse(number)
+    "#{amount(number).capitalize} #{container(number)} of beer on the wall, " +
+    "#{amount(number)} #{container(number)} of beer.\n" +
+    "#{action(number)}, " +
+    "#{amount(successor(number))} #{container(successor(number))} of beer on the wall.\n"
+  end
+
+  def container(number)
+    if number == 1
+      "bottle"
+    else
+      "bottles"
+    end
+  end
+
+  def pronoun(number)
+    if number == 1
+      "it"
+    else
+      "one"
+    end
+  end
+
+  def amount(number)
+    if number == 0
+      "no more"
+    else
+      number.to_s
+    end
+  end
+
+  def action(number)
+    if number == 0
+      "Go to the store and buy some more"
+    else
+      "Take #{pronoun(number)} down and pass it around"
+    end
+  end
+
+  def successor(number)
+    if number == 0
+      99
+    else
+      number - 1
+    end
+  end
+end
